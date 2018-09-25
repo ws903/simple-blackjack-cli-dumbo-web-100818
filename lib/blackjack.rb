6f39c1588bccd376d1b9503e05e064283e9a01bd
David Sim <wsim903@gmail.com>
2018-09-25 20:06:53 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -47,11 +47,6 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-"calls on the #welcome method, 
-  then on the #initial_round method, 
-  then calls #hit? and #display_card_total methods
-  -until- the card sum is greater than 21,
-  then calls on the #end_game method" do
 
 def runner
   # welcome
